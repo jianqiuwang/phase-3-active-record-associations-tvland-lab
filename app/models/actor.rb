3517628cca989dd7662324d930b5dd79408ef29e
Jianqiu Wang <jianqiuwang1@icloud.com>
2022-11-16 14:25:20 -0600

    commit tree 99c0723edbce140994413c571a389dbb76b9691b

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    # puts self.shows.map do |show|
+    #     "#{show.characters.where(actor: self).map do |character|
+    #     character.name
+    # end} - #{show.name}"
+    # end
+
+    self.characters.map do |character|
+        "#{character.name} - #{character.show.name}"
+    end
+  end
 end
